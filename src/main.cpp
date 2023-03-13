ae6b468ef458eb48581ae17ee9b237e5c97ca8ae
it-a-me <wobbe.carter@gmail.com>
2023-03-13 15:31:45 -0700

    port configuration moved to own header

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,7 @@
 #include "rollerSpinner.h"
 #include "spool.h"
 #include "vision.h"
-#define FRONT_LEFT -20
-#define FRONT_RIGHT 11
-#define BACK_LEFT 19
-#define BACK_RIGHT -12
-
-#define ROLLER_SPINNER 1
-
-#define REVERSE -3
-#define FOREWARD 4
-
-#define LOADER -2
-#define PNEUMATICS 'H'
+#include "ports.h"
 // new pros::Motor(-9),
 //                                   new pros::Motor(10),
 //                                   new pros::Motor(-19),
